676bef3040ac449bf332c902d5816cc2edb567c3
Tyler <tvanden@live.com>
2024-05-12 13:37:13 -0600

    make info actually correct to myself

--- my-terminal.js
+++ my-terminal.js
@@ -1,32 +1,20 @@
 const directories = {
-    education: [
-        '',
-        '<white>education</white>',
+    //education: [
+        //'',
+        //'<white>education</white>',
 
-        '* <a href="https://en.wikipedia.org/wiki/Kielce_University_of_Technology">Kielce University of Technology</a> <yellow>"Computer Science"</yellow> 2002-2007 / 2011-2014',
-        '* <a href="https://pl.wikipedia.org/wiki/Szko%C5%82a_policealna">Post-secondary</a> Electronic School <yellow>"Computer Systems"</yellow> 2000-2002',
-        '* Electronic <a href="https://en.wikipedia.org/wiki/Technikum_(Polish_education)">Technikum</a> with major <yellow>"RTV"</yellow> 1995-2000',
-        ''
-    ],
+        //'* <a href="https://en.wikipedia.org/wiki/Kielce_University_of_Technology">Kielce University of Technology</a> <yellow>"Computer Science"</yellow> 2002-2007 / 2011-2014',
+        //'* <a href="https://pl.wikipedia.org/wiki/Szko%C5%82a_policealna">Post-secondary</a> Electronic School <yellow>"Computer Systems"</yellow> 2000-2002',
+        //'* Electronic <a href="https://en.wikipedia.org/wiki/Technikum_(Polish_education)">Technikum</a> with major <yellow>"RTV"</yellow> 1995-2000',
+        //''
+    //],
     projects: [
         '',
         '<white>Open Source projects</white>',
         [
-            ['jQuery Terminal',
-             'https://terminal.jcubic.pl',
-             'library that adds terminal interface to websites'
-            ],
-            ['LIPS Scheme',
-             'https://lips.js.org',
-             'Scheme implementation in JavaScript'
-            ],
-            ['Sysend.js',
-             'https://jcu.bi/sysend',
-             'Communication between open tabs'
-            ],
-            ['Wayne',
-             'https://jcu.bi/wayne',
-             'Pure in browser HTTP requests'
+            ['Bales',
+             'https://github.com/unclassedpenguin/bales',
+             'CLI program to keep track of number of bales fed.'
             ],
         ].map(([name, url, description = '']) => {
             return `* <a href="${url}">${name}</a> &mdash; <white>${description}</white>`;
@@ -36,29 +24,18 @@ const directories = {
     skills: [
         '',
         '<white>languages</white>',
-
         [
             'JavaScript',
-            'TypeScript',
             'Python',
-            'SQL',
-            'PHP',
+            'Go',
             'Bash'
         ].map(lang => `* <yellow>${lang}</yellow>`),
         '',
-        '<white>libraries</white>',
-        [
-            'React.js',
-            'Redux',
-            'Jest',
-        ].map(lib => `* <green>${lib}</green>`),
-        '',
         '<white>tools</white>',
         [
-            'Docker',
             'git',
             'GNU/Linux'
-        ].map(lib => `* <blue>${lib}</blue>`),
+        ].map(lib => `* <yellow>${lib}</yellow>`),
         ''
     ].flat()
 };
@@ -138,7 +115,7 @@ const commands = {
   }
 };
 
-const greetings = 'Tylers Server';
+const greetings = 'Tylers Website';
 const subgreetings = '';
 const font = 'Slant';
 
@@ -206,6 +183,6 @@ function render(text) {
 function ready() {
   term.echo(() => {
     const ascii = render([greetings]);
-    return `${ascii}\nWelcome to my Website! Type '<white>help</white>' for a list of commands.\nThis is based on a <a href="https://www.freecodecamp.org/news/how-to-create-interactive-terminal-based-portfolio/" target="_blank">Tutorial</a>, none of the information within is accurate to me!\n`;
+    return `${ascii}\nWelcome to my Website! Type '<white>help</white>' for a list of commands.\n`;
   });
 }
